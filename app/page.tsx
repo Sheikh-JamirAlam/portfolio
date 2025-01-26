5d73968de6eb2a23c7501d9cf9a72d16fec367db
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-01-26 23:56:32 +0530

    photo added

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,36 @@ export default function Home() {
         </filter>
       </svg>
       <main>
-        <section className="mx-6 mt-6 border-2 border-foreground">
-          <div className="w-[10.5rem] m-4 p-2 bg-white border-2 border-foreground">
-            <p className="text-center leading-[1.25]">
-              Hi there! This is <span className="font-semibold">Sheikh</span>... I am a <br /> Full Stack Developer &amp; Web Designer
-            </p>
+        <section className="h-dvh flex flex-col gap-6">
+          <div className="absolute filter-[url('#noiseFilter')]" />
+          <div className="h-[50%] mx-6 mt-6 border-2 border-foreground">
+            <div className="w-[10.5rem] m-4 p-2 bg-white border-2 border-foreground">
+              <p className="text-center leading-[1.25]">
+                Hi there! This is <span className="font-semibold">Sheikh</span>... I am a <br /> Full Stack Developer &amp; Web Designer.
+              </p>
+            </div>
+            <h1 className="mt-12 text-9xl text-center font-arigato">
+              <RevealText text="Sheikh&nbsp;Jamir&nbsp;Alam" />
+            </h1>
           </div>
-          <h1 className="mt-12 text-9xl text-center font-arigato"><RevealText text="Sheikh&nbsp;Jamir&nbsp;Alam" /></h1>
-          <div className="w-[10rem] h-[10rem] filter-[url('#noiseFilter')]" />
-        </section>
-        <section className="mx-6 mt-6 border-2 border-foreground">
-        <div className="w-[10.5rem] m-4 p-2 bg-white border-2 border-foreground">
-            <p className="text-center leading-[1.25]">
-              Hi there! This is <span className="font-semibold">Sheikh</span>... I am a <br /> Full Stack Developer &amp; Web Designer
-            </p>
+          <div className="h-[50%] mx-6 mb-6 flex gap-6">
+            <div className="w-[25%] border-2 border-foreground"></div>
+            <div id="pic-container" className="w-[25%] border-2 border-foreground relative flex flex-row-reverse justify-between">
+              <div className="w-24 h-fit m-6 p-2 relative bg-white border-2 border-foreground">
+                <p className="text-center leading-[1.25]">Here I am! With the nerd glasses!</p>
+              </div>
+              <div className="w-24 h-fit m-6 p-2 relative bg-white border-2 border-foreground">
+                <p className="text-center leading-[1.25]">Here I am! With the nerd glasses!</p>
+              </div>
+            </div>
+            <div className="w-[50%] flex flex-row-reverse justify-between border-2 border-foreground bg-cover bg-center" style={{ backgroundImage: "url('images/map.png')" }}>
+              <div className="w-36 h-fit m-4 p-2 bg-white border-2 border-foreground">
+                <p className="text-center leading-[1.25]">And here is my city of Joy...</p>
+              </div>
+              <div className="w-36 mt-auto m-4 p-2 bg-white border-2 border-foreground">
+                <p className="text-center leading-[1.25]">...best place to get Biriyani. Thank me later!</p>
+              </div>
+            </div>
           </div>
         </section>
       </main>
